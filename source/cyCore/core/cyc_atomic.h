99684d394df0f72b06c5e09b59b8f7c743201a9e
thejinchao <thejinchao@gmail.com>
2017-02-24 19:39:20 +0800

    add atomic_bool_t type

--- source/cyCore/core/cyc_atomic.h
+++ source/cyCore/core/cyc_atomic.h
@@ -14,6 +14,7 @@ typedef std::atomic<int64_t>	atomic_int64_t;
 typedef std::atomic<uint32_t>	atomic_uint32_t;
 typedef std::atomic<uint64_t>	atomic_uint64_t;
 typedef std::atomic<void*>		atomic_ptr_t;
+typedef std::atomic<bool>		atomic_bool_t;
 
 template<typename T>
 inline bool atomic_compare_exchange(std::atomic<T>& obj, T expected, T desired) {
