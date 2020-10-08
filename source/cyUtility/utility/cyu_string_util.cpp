4ec57dd7bc6a9dc3034747df8a6b42e20c03e00a
thejinchao <thejinchao@gmail.com>
2020-10-08 21:14:04 +0800

    add string utility functions

---
+++ source/cyUtility/utility/cyu_string_util.cpp
@@ -0,0 +1,40 @@
+﻿#include "cyu_string_util.h"
+
+namespace cyclone
+{
+namespace string_util
+{
+
+//-------------------------------------------------------------------------------------
+std::string size_to_string(float s)
+{
+	char temp[64] = { 0 };
+
+	static const size_t KB = 1024;
+	static const size_t MB = 1024 * 1024;
+	static const size_t GB = 1024 * 1024 * 1024;
+
+	if (s < KB) {
+		std::snprintf(temp, 64, "%.2f ", s);
+	}
+	else if (s < MB) {
+		std::snprintf(temp, 64, "%.2f KB", (float)s / (float)(KB));
+	}
+	else if (s < GB) {
+		std::snprintf(temp, 64, "%.2f MB", (float)s / (float)(MB));
+	}
+	else {
+		std::snprintf(temp, 64, "%.2f GB", (float)s / (float)(GB));
+	}
+	
+	return std::string(temp);
+}
+
+//-------------------------------------------------------------------------------------
+std::string size_to_string(size_t s)
+{
+	return size_to_string((float)s);
+}
+
+}
+}
---
+++ source/cyUtility/utility/cyu_string_util.h
@@ -0,0 +1,21 @@
+﻿/*
+Copyright(C) thecodeway.com
+*/
+#ifndef _CYCLONE_UTILITY_STRING_UTIL_H_
+#define _CYCLONE_UTILITY_STRING_UTIL_H_
+
+#include <cyclone_config.h>
+
+namespace cyclone
+{
+namespace string_util
+{
+
+// convert size to string with 'KB', 'MB', 'GB' tail
+std::string size_to_string(float s);
+std::string size_to_string(size_t s);
+
+}
+}
+
+#endif
