c48ccd17d2271e64d9429ab97a80a509ce99073a
thejinchao <thejinchao@gmail.com>
2022-06-09 17:19:03 +0800

    pre-merge from master

--- source/cyNetwork/cy_network.h
+++ source/cyNetwork/cy_network.h
@@ -1,15 +1,12 @@
 ﻿/*
 Copyright(C) thecodeway.com
 */
-#ifndef _CYCLONE_NETWORK_H_
-#define _CYCLONE_NETWORK_H_
+#pragma once
 
 #include <cyclone_config.h>
 
 #include <network/cyn_address.h>
 #include <network/cyn_tcp_server.h>
+#include <network/cyn_udp_server.h>
 #include <network/cyn_tcp_connection.h>
 #include <network/cyn_tcp_client.h>
-#include <network/cyn_udp_server.h>
-
-#endif
