fbeddc9826c2d1042f1bb3d49d93d30c1d88f61c
thejinchao <thejinchao@gmail.com>
2026-01-02 11:55:32 +0800

    Use the `REQUIRE_RANGE` macro instead of the combination of the original `REQUIRE_GE` and `REQUIRE_LE` macros

--- test/unit/cyt_unit_utils.h
+++ test/unit/cyt_unit_utils.h
@@ -6,7 +6,7 @@
 #define REQUIRE_GT(a, b)			REQUIRE((a) > (b))
 #define REQUIRE_LE(a, b)			REQUIRE((a) <= (b))
 #define REQUIRE_GE(a, b)			REQUIRE((a) >= (b))
-#define REQUIRE_RANCE(a, min, max)	REQUIRE(((a) >= (min) && (a)<=(max)))
+#define REQUIRE_RANGE(a, min, max)	REQUIRE((a) >= (min)); REQUIRE((a)<=(max));
 #define REQUIRE_TRUE(a)				REQUIRE((a))
 #define REQUIRE_STREQ(a, b)			REQUIRE_THAT((a), Catch::Matchers::Equals(b))
 #define PRINT_CURRENT_TEST_NAME()	printf("%s\n", Catch::getCurrentContext().getResultCapture()->getCurrentTestName().c_str())
