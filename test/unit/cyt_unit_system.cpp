775105ae84a4ca9d75d5b66af27a63b47da5dc7d
thejinchao <thejinchao@gmail.com>
2026-01-03 16:15:11 +0800

    Add atomic stress test case

--- test/unit/cyt_unit_system.cpp
+++ test/unit/cyt_unit_system.cpp
@@ -56,3 +56,70 @@ TEST_CASE("System atomic test", "[System][Atomic]")
 	REQUIRE_EQ(a.load(), 4);
 }
 
+struct AtomicTestThreadData
+{
+	atomic_int32_t* a;
+	int32_t loop_counts;
+	sys_api::signal_t begin_signal;
+};
+
+//-------------------------------------------------------------------------------------
+static void _fetchAddThread(void* param)
+{
+	AtomicTestThreadData* threadData = (AtomicTestThreadData*)param;
+	sys_api::signal_wait(threadData->begin_signal);
+
+	for(int32_t i=0; i<threadData->loop_counts; i++)
+	{
+		threadData->a->fetch_add(1);
+	}
+}
+
+//-------------------------------------------------------------------------------------
+static void _fetchSubThread(void* param)
+{
+	AtomicTestThreadData* threadData = (AtomicTestThreadData*)param;
+	sys_api::signal_wait(threadData->begin_signal);
+
+	for (int32_t i = 0; i < threadData->loop_counts; i++)
+	{
+		threadData->a->fetch_sub(1);
+	}
+}
+
+//-------------------------------------------------------------------------------------
+TEST_CASE("System atomic stress test", "[System][Atomic][Stress]")
+{
+	PRINT_CURRENT_TEST_NAME();
+
+	const int32_t k_thread_counts = sys_api::get_cpu_counts() < 16 ? 16 : sys_api::get_cpu_counts();
+
+	atomic_int32_t a(0xFACEC0DE);
+
+	AtomicTestThreadData threadData;
+	threadData.begin_signal = sys_api::signal_create(true);
+	threadData.a = &a;
+	threadData.loop_counts = 10000;
+	
+	thread_t* fetchAddThread = new thread_t[k_thread_counts];
+	thread_t* fetchSubThread = new thread_t[k_thread_counts];
+	for (int32_t i = 0; i < k_thread_counts; i++)
+	{
+		fetchAddThread[i] = sys_api::thread_create(_fetchAddThread, &threadData, nullptr);
+		fetchSubThread[i] = sys_api::thread_create(_fetchSubThread, &threadData, nullptr);
+	}
+
+	//begin all threads
+	sys_api::signal_notify(threadData.begin_signal);
+
+	for (int32_t i = 0; i < k_thread_counts; i++)
+	{
+		sys_api::thread_join(fetchAddThread[i]);
+		sys_api::thread_join(fetchSubThread[i]);
+	}
+
+	REQUIRE_EQ(threadData.a->load(), 0xFACEC0DE);
+	
+	delete[] fetchAddThread;
+	delete[] fetchSubThread;
+}
